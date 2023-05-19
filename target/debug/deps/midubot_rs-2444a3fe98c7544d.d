9748296aa7399e5664b606ef2882c915dba17453
lemin-n <emiliorccp@gmail.com>
2023-05-18 21:02:29 -0300

    Add readme example to main.rs

--- target/debug/deps/midubot_rs-2444a3fe98c7544d.d
+++
@@ -1,5 +0,0 @@
-/home/lemi/rust_proyects/midubot-rs/target/debug/deps/midubot_rs-2444a3fe98c7544d.rmeta: src/main.rs
-
-/home/lemi/rust_proyects/midubot-rs/target/debug/deps/midubot_rs-2444a3fe98c7544d.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/midubot_rs-5f0b1a62e6514c6a.d
+++
@@ -1,5 +0,0 @@
-/home/lemi/rust_proyects/midubot-rs/target/debug/deps/midubot_rs-5f0b1a62e6514c6a.rmeta: src/main.rs
-
-/home/lemi/rust_proyects/midubot-rs/target/debug/deps/midubot_rs-5f0b1a62e6514c6a.d: src/main.rs
-
-src/main.rs:
